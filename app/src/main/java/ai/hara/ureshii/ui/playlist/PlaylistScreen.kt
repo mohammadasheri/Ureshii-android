24076c110165d9a26fd7b8965bdaf0076fa1df4c
Mohammad Asheri <mhm.asheri@gmail.com>
2024-01-01 14:23:15 +0330

    add playlist screen

--- app/src/main/java/ai/hara/ureshii/ui/playlist/PlaylistScreen.kt
+++ app/src/main/java/ai/hara/ureshii/ui/playlist/PlaylistScreen.kt
@@ -2,11 +2,8 @@ package ai.hara.ureshii.ui.playlist
 
 import ai.hara.ureshii.R
 import ai.hara.ureshii.data.model.Playlist
+import ai.hara.ureshii.ui.theme.ColorNavigation
 import ai.hara.ureshii.util.getHostURL
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
@@ -17,20 +14,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -39,13 +27,10 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import timber.log.Timber
 
 private val headerHeight = 275.dp
 private val toolbarHeight = 56.dp
@@ -59,14 +44,11 @@ fun PlaylistScreen(playlist: Playlist) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(
-                MaterialTheme.colorScheme.surface
-            )
+            .background(ColorNavigation)
     ) {
         Header(playlist, scroll, headerHeightPx)
         Body(scroll)
         Toolbar(scroll, headerHeightPx, toolbarHeightPx)
-//        Title()
     }
 }
 
@@ -100,50 +82,6 @@ private fun Header(playlist: Playlist, scroll: ScrollState, headerHeightPx: Floa
     }
 }
 
-
-@Composable
-private fun Toolbar(scroll: ScrollState, headerHeightPx: Float, toolbarHeightPx: Float) {
-    val toolbarBottom = headerHeightPx - toolbarHeightPx
-    val showToolbar = remember {
-        derivedStateOf {
-            Timber.tag("asdfasf").i("asdfasdf")
-            scroll.value >= toolbarBottom
-        }
-    }
-
-    AnimatedVisibility(
-        visible = showToolbar.value,
-        enter = fadeIn(animationSpec = tween(300)),
-        exit = fadeOut(animationSpec = tween(300))
-    ) {
-
-        TopAppBar(
-            modifier = Modifier.background(
-                brush = Brush.horizontalGradient(
-                    listOf(Color(0xff026586), Color(0xff032C45))
-                )
-            ),
-            navigationIcon = {
-                IconButton(
-                    onClick = {},
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .size(36.dp)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Menu,
-                        contentDescription = "",
-                        tint = Color.White
-                    )
-                }
-            },
-            title = { Text(text = "Mohammad")},
-            backgroundColor = Color.Transparent,
-            elevation = 0.dp
-        )
-    }
-}
-
 @Composable
 private fun Body(scroll: ScrollState) {
     Column(
@@ -166,14 +104,3 @@ private fun Body(scroll: ScrollState) {
         }
     }
 }
-
-//@Composable
-//private fun Title() {
-//    Text(
-//        text = "New York",
-//        fontSize = 30.sp,
-//        fontWeight = FontWeight.Bold,
-//        textAlign = TextAlign.Center,
-//        modifier = Modifier.fillMaxWidth()
-//    )
-//}
--- app/src/main/java/ai/hara/ureshii/ui/playlist/Toolbar.kt
+++ app/src/main/java/ai/hara/ureshii/ui/playlist/Toolbar.kt
@@ -1,62 +1,62 @@
-//package ai.hara.ureshii.ui.playlist
-//
-//import ai.hara.ureshii.ui.theme.ColorBottomPlayer
-//import androidx.compose.animation.AnimatedVisibility
-//import androidx.compose.animation.core.tween
-//import androidx.compose.animation.fadeIn
-//import androidx.compose.animation.fadeOut
-//import androidx.compose.foundation.ScrollState
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.foundation.layout.size
-//import androidx.compose.material.IconButton
-//import androidx.compose.material.TopAppBar
-//import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.Menu
-//import androidx.compose.material3.Icon
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.derivedStateOf
-//import androidx.compose.runtime.remember
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.unit.dp
-//
-//
-//@Composable
-//fun Toolbar(scroll: ScrollState, headerHeightPx: Float, toolbarHeightPx: Float) {
-//    val toolbarBottom = headerHeightPx - toolbarHeightPx
-//    val showToolbar = remember {
-//        derivedStateOf {
-//            scroll.value >= toolbarBottom
-//        }
-//    }
-//
-//    AnimatedVisibility(
-//        visible = showToolbar.value,
-//        enter = fadeIn(animationSpec = tween(300)),
-//        exit = fadeOut(animationSpec = tween(300))
-//    ) {
-//
-//        TopAppBar(
-//            modifier = Modifier.background(ColorBottomPlayer),
-//            navigationIcon = {
-//                IconButton(
-//                    onClick = {},
-//                    modifier = Modifier
-//                        .padding(horizontal = 16.dp)
-//                        .size(36.dp)
-//                ) {
-//                    Icon(
-//                        imageVector = Icons.Default.Menu,
-//                        contentDescription = "",
-//                        tint = Color.White
-//                    )
-//                }
-//            },
-//            title = { Text(text = "Mohammad") },
-//            backgroundColor = Color.Transparent,
-//            elevation = 0.dp
-//        )
-//    }
-//}
+package ai.hara.ureshii.ui.playlist
+
+import ai.hara.ureshii.ui.theme.ColorBottomPlayer
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun Toolbar(scroll: ScrollState, headerHeightPx: Float, toolbarHeightPx: Float) {
+    val toolbarBottom = headerHeightPx - toolbarHeightPx
+    val showToolbar = remember {
+        derivedStateOf {
+            scroll.value >= toolbarBottom
+        }
+    }
+
+    AnimatedVisibility(
+        visible = showToolbar.value,
+        enter = fadeIn(animationSpec = tween(300)),
+        exit = fadeOut(animationSpec = tween(300))
+    ) {
+
+        TopAppBar(
+            modifier = Modifier.background(ColorBottomPlayer),
+            navigationIcon = {
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .size(36.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "",
+                        tint = Color.White
+                    )
+                }
+            },
+            title = { Text(text = "Mohammad") },
+            backgroundColor = Color.Transparent,
+            elevation = 0.dp
+        )
+    }
+}
